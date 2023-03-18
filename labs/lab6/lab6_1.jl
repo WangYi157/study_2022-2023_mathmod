a66ac2e0daa5cfe0fb16ebf609c4d51aa5dd9aa6
vani <vani@Linux.myguest.virtualbox.org>
2023-03-18 23:05:16 +0300

    feat(lab6): add lab6 code files, report, presentation

---
+++ labs/lab6/lab6_1.jl
@@ -0,0 +1,43 @@
+using Plots
+using DifferentialEquations
+
+alfa = 0.7
+betta = 0.3
+T = (0.0, 30.0)
+N = 14041
+I0 = 131
+R0 = 71
+I_1 = 150
+I_2 = 100
+S0 = N - I0 - R0
+u0 = [S0, I0, R0]
+
+function F!(du, u, p, t)
+    if u[2] > I_1
+        du[1] = -alfa*u[1]
+        du[2] = alfa*u[1] - betta*u[2]
+    else 
+        du[1] = 0
+        du[2] = -betta*u[2]
+    end
+    du[3] = betta * u[2]
+end 
+
+pr1 = ODEProblem(F!, u0, T)
+sol = solve(pr1, dtmax=0.2)
+
+ss1 = []
+ii1 = []
+rr1 = []
+tt1 = sol.t 
+
+for u in sol.u 
+    s, i, r = u
+    push!(ss1, s)
+    push!(ii1, i)
+    push!(rr1, r)
+end 
+
+plt1 = plot(dpi=150, size = (800, 400))
+plot!(plt1, tt1, [ss1, ii1, rr1], color = [:red :green :blue], xlabel="time", label = ["S(t)" "I(t)" "R(t)"])
+savefig(plt1, "img1jl.png")
---
+++ labs/lab6/lab6_1.mo
@@ -0,0 +1,22 @@
+model lab6_1
+  constant Real alfa = 0.7;
+  constant Real betta = 0.3;
+  constant Integer N = 14041;
+  constant Integer I_1 = 150;
+  Real S(time);
+  Real I(time);
+  Real R(time);
+initial equation
+  I = 131;
+  R = 71;
+  S = N - I - R;
+equation
+  if I > I_1 then
+    der(S) = -alfa*S;
+    der(I) = alfa*S - betta*I;
+  else 
+    der(S) = 0;
+    der(I) = -betta*I;
+  end if;
+  der(R) = betta*I;
+end lab6_1;
